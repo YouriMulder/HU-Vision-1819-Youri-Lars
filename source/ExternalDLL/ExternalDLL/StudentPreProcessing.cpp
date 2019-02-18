2427f09970f59399dc604aea7430ce356ce809b7
Youri <Youri@demuldertjes.nl>
2019-02-18 17:43:01 +0100

    Gaussion filter added

--- source/ExternalDLL/ExternalDLL/StudentPreProcessing.cpp
+++ source/ExternalDLL/ExternalDLL/StudentPreProcessing.cpp
@@ -1,5 +1,7 @@
 #include "StudentPreProcessing.h"
 
+#include "ImageFactory.h"
+#include "Kernel.h"
 
 IntensityImage * StudentPreProcessing::stepToIntensityImage(const RGBImage &image) const {
 	return nullptr;
@@ -9,8 +11,27 @@ IntensityImage * StudentPreProcessing::stepScaleImage(const IntensityImage &imag
 	return nullptr;
 }
 
-IntensityImage * StudentPreProcessing::stepEdgeDetection(const IntensityImage &image) const {
-	return nullptr;
+IntensityImage * StudentPreProcessing::stepEdgeDetection(const IntensityImage &image) const {	
+	// input scaled intensity image 
+	// output Edge-detected image
+
+	/* 
+	void set(const int width, const int height);
+
+	void setPixel(int x, int y, Intensity pixel);
+	void setPixel(int i, Intensity pixel);
+
+	Intensity getPixel(int x, int y) const;
+	Intensity getPixel(int i) const1;
+	
+	*/
+	IntensityImage* edgeImage = ImageFactory::newIntensityImage(image);
+	
+	// gausian filter
+	double guassian[5][5];
+	Kernel::FilterCreation(guassian);
+	Kernel::apply(image, edgeImage, 5, 5, guassian);
+	return edgeImage;
 }
 
 IntensityImage * StudentPreProcessing::stepThresholding(const IntensityImage &image) const {
