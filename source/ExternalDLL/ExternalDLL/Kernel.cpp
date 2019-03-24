48bf3cf6b264aea30be29ef0f8627a86b565647b
Youri <Youri@demuldertjes.nl>
2019-03-24 12:08:36 +0100

    IntensityImage to double vectors

--- source/ExternalDLL/ExternalDLL/Kernel.cpp
+++ source/ExternalDLL/ExternalDLL/Kernel.cpp
@@ -1,39 +1,54 @@
 #include "Kernel.h"
 
-#include <cmath>
-#include <iomanip>
-#include <iostream>
-#include <algorithm>
-#include <array>
-
 #include "ImageFactory.h"
-/*void Kernel::apply(const IntensityImage &sourceImage, IntensityImage *destinationImage, int kWidth, int kHeight, double kernel[5][5]) {
-	 
-}*/
 
-void Kernel::createGuassianKernel(double GKernel[][5], float sigma) {
-	// intialising standard deviation to 1.0 
+#include <vector>
+
+std::vector<std::vector<double>> Kernel::applyGuassian(const IntensityImage &image) {
+	const int kernelSize = 5;
+	double gaussianKernel[kernelSize][kernelSize] = {};
+	const double pi = 3.14159265358979323846;
+	// set standard deviation to 1.0
+	double sigma = 1.6;
 	double r, s = 2.0 * sigma * sigma;
 
-	// sum is for normalization 
+	// sum is for normalization
 	double sum = 0.0;
 
-	// generating 5x5 kernel 
+	// generate 5x5 kernel
 	for (int x = -2; x <= 2; x++) {
 		for (int y = -2; y <= 2; y++) {
-			r = std::sqrt(x * x + y * y);
-			GKernel[x + 2][y + 2] = (std::exp(-(r * r) / s)) / (3.1415296 * s);
-			sum += GKernel[x + 2][y + 2];
+			r = sqrt(x*x + y * y);
+			gaussianKernel[x + 2][y + 2] = (exp(-(r*r) / s)) / (pi * s);
+			sum += gaussianKernel[x + 2][y + 2];
 		}
 	}
 
-	// normalising the Kernel 
-	for (int i = 0; i < 5; ++i)
-		for (int j = 0; j < 5; ++j)
-			GKernel[i][j] /= sum;
+	// normalize the Kernel
+	for (int i = 0; i < 5; ++i) {
+		for (int j = 0; j < 5; ++j) {
+			gaussianKernel[i][j] /= sum;
+		}
+	}
+
+	int size = kernelSize/2;
+	std::vector<std::vector<double>> output(image.getHeight() + 1 - kernelSize, std::vector<double>(image.getWidth() + 1 - kernelSize, 0));
+	for(int pixelX = size; pixelX < image.getWidth() - size; ++pixelX) {
+		for(int pixelY = size; pixelY < image.getHeight() - size; ++pixelY) {
+			double sum = 0;
+
+			for (int x = 0; x < kernelSize; ++x) {
+				for (int y = 0; y < kernelSize; ++y) {
+					sum += gaussianKernel[x][y] * (double)(image.getPixel(pixelX + x - size, pixelY + y - size));
+				}
+			}
+			output[pixelY - size][pixelX - size] = sum;
+		}
+	}
+	return std::move(output);
 }
 
-void Kernel::sobelFilter(const IntensityImage &sourceImage, std::vector<std::vector<float>> &destImage, std::vector<std::vector<float>> &directionImage) {
+void Kernel::sobelFilter(const std::vector<std::vector<double>> &sourceImage, std::vector<std::vector<double>> &destImage, std::vector<std::vector<double>> &directionImage) {
 	const int KERNEL_X = 3;
 	const int KERNEL_Y = 3;
 
@@ -55,10 +70,10 @@ void Kernel::sobelFilter(const IntensityImage &sourceImage, std::vector<std::vec
 	int kOffsetX = KERNEL_X / 2;
 	int kOffsetY = KERNEL_Y / 2;
 
-	float max = 0;
+	double max = 0;
 
-	for (int y = 1; y < sourceImage.getHeight()-1; ++y) {
-		for (int x = 1; x < sourceImage.getWidth()-1; ++x) {
+	for (int y = 1; y < sourceImage.size() - 1; ++y) {
+		for (int x = 1; x < sourceImage[0].size() - 1; ++x) {
 			accumulatorVertical = 0;
 			accumulatorHorizontal = 0;
 			int topLeftX = x - kOffsetX;
@@ -66,14 +81,14 @@ void Kernel::sobelFilter(const IntensityImage &sourceImage, std::vector<std::vec
 
 			for (int kernelY = 0; kernelY < KERNEL_Y; ++kernelY) {
 				for (int kernelX = 0; kernelX < KERNEL_X; ++kernelX) {
-					accumulatorVertical += sourceImage.getPixel(topLeftX + kernelX, topLeftY + kernelY) * kernelVertical[kernelY][kernelX];
-					accumulatorHorizontal += sourceImage.getPixel(topLeftX + kernelX, topLeftY + kernelY) * kernelHorizontal[kernelY][kernelX];
+					accumulatorVertical += sourceImage[topLeftY + kernelY][topLeftX + kernelX] * kernelVertical[kernelY][kernelX];
+					accumulatorHorizontal += sourceImage[topLeftY + kernelY][topLeftX + kernelX] * kernelHorizontal[kernelY][kernelX];
 				}
 			}
 
-			float gradient = std::sqrt((accumulatorHorizontal * accumulatorHorizontal) + (accumulatorVertical * accumulatorVertical));
+			double gradient = std::sqrt((accumulatorHorizontal * accumulatorHorizontal) + (accumulatorVertical * accumulatorVertical));
 			destImage[y][x] = gradient;
-			
+
 			max = destImage[y][x] > max ? destImage[y][x] : max;
 			directionImage[y][x] = std::atan2(accumulatorVertical, accumulatorHorizontal);
 		}
@@ -84,48 +99,28 @@ void Kernel::sobelFilter(const IntensityImage &sourceImage, std::vector<std::vec
 			value = 255.0f * value / max;
 		}
 	}
-
-
-
 }
 
-double Kernel::getDegrees(double radians) {
-	return (radians * 180) / 3.14;
-}
-
-double Kernel::roundAngle(double angleDeg) {
-	if ((0 <= angleDeg && angleDeg < 22.5) || (157.5 <= angleDeg && angleDeg < 180)) {
-		angleDeg = 0;
-	} else if (22.5 <= angleDeg && angleDeg < 67.5) {
-		angleDeg = 45;
-	} else if (67.5 <= angleDeg && angleDeg < 112.5) {
-		angleDeg = 90;
-	} else if (112.5 <= angleDeg && angleDeg < 157.5) {
-		angleDeg = 135;
-	}
-	return angleDeg;
-}
-
-void Kernel::nonMaxSupp(std::vector<std::vector<float>> &image, std::vector<std::vector<float>> &angle) {
+void Kernel::nonMaxSupp(std::vector<std::vector<double>> &image, std::vector<std::vector<double>> &angle) {
 
 	auto height = image.size() > angle.size() ? image.size() : angle.size();
 	auto width = image[0].size() > angle[0].size() ? image[0].size() : angle[0].size();
-	std::vector<std::vector<float>> dest(image.size(), std::vector<float>(image[0].size(), 0));
+	std::vector<std::vector<double>> dest(image.size(), std::vector<double>(image[0].size(), 0));
 	for (int y = 1; y < height - 1; ++y) {
 		for (int x = 1; x < width - 1; ++x) {
-			float theta = getDegrees(angle[y][x]) + 180;
+			double theta = (angle[y][x] * 180) / 3.14 + 180;
 			//std::cout << theta << "\n";
 			// 0 degrees
 			if ((theta >= 0 && theta <= 22.5) ||
 				(theta >= 337.5 && theta <= 360)) {
-				if ( (image[y][x] >= image[y][x - 1]) && (image[y][x] >= image[y][x + 1]) ) {
+				if ((image[y][x] >= image[y][x - 1]) && (image[y][x] >= image[y][x + 1])) {
 					dest[y][x] = image[y][x];
 				}
 			}
 			// 45 degrees
 			else if ((theta >= 22.5 && theta <= 67.5) ||
 				(theta >= 202.5 && theta <= 247.5)) {
-				if ((image[y][x] >= image[y - 1][x+1]) && (image[y][x] >= image[y+1][x-1])) {
+				if ((image[y][x] >= image[y - 1][x + 1]) && (image[y][x] >= image[y + 1][x - 1])) {
 					dest[y][x] = image[y][x];
 				}
 			}
@@ -139,7 +134,7 @@ void Kernel::nonMaxSupp(std::vector<std::vector<float>> &image, std::vector<std:
 			// 135 degrees
 			else if ((theta >= 112.5 && theta <= 157.5) ||
 				(theta >= 292.5 && theta <= 337.5)) {
-				if ((image[y][x] >= image[y-1][x+1]) && (image[y][x] >= image[y + 1][x + 1])) {
+				if ((image[y][x] >= image[y - 1][x + 1]) && (image[y][x] >= image[y + 1][x + 1])) {
 					dest[y][x] = image[y][x];
 				}
 			}
@@ -149,34 +144,37 @@ void Kernel::nonMaxSupp(std::vector<std::vector<float>> &image, std::vector<std:
 	image = dest;
 }
 
-void Kernel::doubleThreshold(std::vector<std::vector<float>> &image, const Intensity& lowThreshold, const Intensity& highThreshold, const Intensity& strong, const Intensity& weak) {
+void Kernel::doubleThreshold(std::vector<std::vector<double>> &image, const Intensity& lowThreshold, const Intensity& highThreshold, const Intensity& strong, const Intensity& weak) {
 	for (int y = 0; y < image.size(); ++y) {
 		for (int x = 0; x < image[y].size(); ++x) {
 			auto value = image[y][x];
 			if (value > highThreshold) {
 				image[y][x] = strong;
-			} else if (value >= lowThreshold && value <= highThreshold) {
+			}
+			else if (value >= lowThreshold && value <= highThreshold) {
 				image[y][x] = weak;
-			} else {
+			}
+			else {
 				image[y][x] = 0;
 			}
 		}
 	}
 }
 
-void Kernel::tracking(std::vector<std::vector<float>> &image, const Intensity &weak, const Intensity &strong) {
+void Kernel::tracking(std::vector<std::vector<double>> &image, const Intensity &weak, const Intensity &strong) {
 	for (int y = 1; y < image.size() - 1; ++y) {
 		for (int x = 1; x < image[y].size() - 1; ++x) {
 			auto value = image[y][x];
 			if (value == weak) {
-				if (image[y][x + 1] == strong 
+				if (image[y][x + 1] == strong
 					|| image[y][x - 1] == strong
 					|| image[y + 1][x] == strong
 					|| image[y - 1][x] == strong
 					|| image[y + 1][x + 1] == strong
 					|| image[y - 1][x - 1] == strong) {
-							image[y][x] = strong;
-				} else {
+					image[y][x] = strong;
+				}
+				else {
 					image[y][x] = 0;
 				}
 			}
@@ -184,7 +182,7 @@ void Kernel::tracking(std::vector<std::vector<float>> &image, const Intensity &w
 	}
 }
 
-void Kernel::toHistogram(const std::vector<std::vector<float>> &image, std::array<float, 256>& histogram) {
+void Kernel::toHistogram(const std::vector<std::vector<double>> &image, std::array<double, 256>& histogram) {
 	histogram.fill(0);
 	for (const auto row : image) {
 		for (const int value : row) {
@@ -193,15 +191,15 @@ void Kernel::toHistogram(const std::vector<std::vector<float>> &image, std::arra
 	}
 }
 
-double Kernel::otsu(const std::vector<std::vector<float>> &image, std::array<float, 256>& histogram) {
+double Kernel::otsu(const std::vector<std::vector<double>> &image, std::array<double, 256>& histogram) {
 	int threshold = 0;
 
 	int total = image.size() * image[0].size();
-	
-	float sum = 0;
-	float sumB = 0;
+
+	double sum = 0;
+	double sumB = 0;
 	int wB = 0;
-	float maximum = 0;
+	double maximum = 0;
 
 	for (int i = 0; i < histogram.size(); ++i) {
 		sum += i * histogram[i];
@@ -214,12 +212,12 @@ double Kernel::otsu(const std::vector<std::vector<float>> &image, std::array<flo
 
 		sumB += i * histogram[i];
 		auto mF = (sum - sumB) / wF;
-		float between = wB * wF * ((sumB / wB) - mF) * ((sumB / wB) - mF);
+		double between = wB * wF * ((sumB / wB) - mF) * ((sumB / wB) - mF);
 		if (between >= maximum) {
 			threshold = i;
 			maximum = between;
 		}
 	}
-	
+
 	return threshold;
 }
--- source/ExternalDLL/ExternalDLL/StudentPreProcessing.cpp
+++ source/ExternalDLL/ExternalDLL/StudentPreProcessing.cpp
@@ -3,9 +3,7 @@
 #include "ImageFactory.h"
 #include "Kernel.h"
 
-#include <cmath>
 #include <iostream>
-#include <array>
 
 IntensityImage * StudentPreProcessing::stepToIntensityImage(const RGBImage &image) const {
 	return nullptr;
@@ -16,37 +14,31 @@ IntensityImage * StudentPreProcessing::stepScaleImage(const IntensityImage &imag
 }
 
 IntensityImage * StudentPreProcessing::stepEdgeDetection(const IntensityImage &image) const {
-	// input scaled intensity image 
-	// output Edge-detected image
-
-	IntensityImage* edgeImage = ImageFactory::newIntensityImage(image);
-
-	// gausian filter
-	double guassian[5][5];
-	Kernel::createGuassianKernel(guassian, 0.33f);
-	//Kernel::apply(image, *edgeImage, 5, 5, guassian);
+	// gaussian
+	std::vector<std::vector<double>> guassian = Kernel::applyGuassian(image);
 	
-	std::vector<std::vector<float>> picture(edgeImage->getHeight(), std::vector<float>(edgeImage->getWidth(), 0));
-	std::vector<std::vector<float>> directions(edgeImage->getHeight(), std::vector<float>(edgeImage->getWidth(), 0));
+	// sobel
+	std::vector<std::vector<double>> directions(guassian.size(), std::vector<double>(guassian[0].size(), 0));
+	std::vector<std::vector<double>> sobel(guassian.size(), std::vector<double>(guassian[0].size(), 0));
+	Kernel::sobelFilter(guassian, sobel, directions);
 
-	Kernel::sobelFilter(*edgeImage, picture, directions);
-	std::array<float, 256> histogram;
-
-	Kernel::toHistogram(picture, histogram);
+	// non max
+	Kernel::nonMaxSupp(sobel, directions);
+	
+	std::array<double, 256> histogram;
+	Kernel::toHistogram(sobel, histogram);
 	const Intensity strong = 255;
 	const Intensity weak = 100;
-	const Intensity highTres = Kernel::otsu(picture, histogram);
-	const Intensity lowTres = highTres/2;
+	const Intensity highTres = Kernel::otsu(sobel, histogram);
+	const Intensity lowTres = highTres / 2;
+	//Kernel::doubleThreshold(sobel, lowTres, highTres, strong, weak);
+	
+	//Kernel::tracking(sobel, weak, strong);
 
-	std::cout << "Treshold : " << double(highTres) << "\n";
-	//Kernel::nonMaxSupp(picture, directions);
-	Kernel::doubleThreshold(picture, lowTres, highTres, strong, weak);
-	Kernel::tracking(picture, weak, strong);
-
-	IntensityImage* img = ImageFactory::newIntensityImage(picture[0].size(), picture.size());
+	IntensityImage* img = ImageFactory::newIntensityImage(sobel[0].size(), sobel.size());
 	for (int y = 0; y < img->getHeight(); ++y) {
 		for (int x = 0; x < img->getWidth(); ++x) {
-			img->setPixel(x, y, picture[y][x]);
+			img->setPixel(x, y, sobel[y][x]);
 		}
 	}
 	return img;
